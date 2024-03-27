c4cda82e4d9bffd6249ab193f8fa26c66905c98b
eyamhamdi03 <eyamhamdi2003@gmail.com>
2024-03-27 16:30:17 +0100

    created assets folder assets/photo

--- Script.js
+++ Script.js
@@ -76,10 +76,10 @@ fetch('footer.html')
     });
 
 var cards = [
-    { name: "Chicken Salad", price: "10.25 DT", photo: "Food Photo.png" },
-    { name: "Margherita Pizza", price: "12.50 DT", photo: "2.png" },
-    { name: "Chicken Alfredo Pasta", price: "15.75 DT", photo: "3.png" },
-    { name: "Classic Cheeseburger", price: "8.99 DT", photo: "4.png" },
+    { name: "Chicken Salad", price: "10.25 DT", photo: "assets/Food Photo.png" },
+    { name: "Margherita Pizza", price: "12.50 DT", photo: "assets/2.png" },
+    { name: "Chicken Alfredo Pasta", price: "15.75 DT", photo: "assets/3.png" },
+    { name: "Classic Cheeseburger", price: "8.99 DT", photo: "assets/4.png" },
 ];
 
 var menuRow = document.getElementById("menuRow");
