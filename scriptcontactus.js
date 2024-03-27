7dd514de73c3226f33a842571cb71599543ac9a2
RoumaissaFezai <roumaissafezai@gmail.com>
2024-03-27 16:31:08 +0100

    scriptcontactus.js modified

--- scriptcontactus.js
+++ scriptcontactus.js
@@ -1,23 +1,36 @@
-
 function resetForm() {
     document.getElementById("contactForm").reset();
-    }
+}
+
 function showSuccessMessage() {
     document.getElementById("successMessage").style.display = "block";
-    }
+}
+
 document.getElementById("contactForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    fetch(this.action, {
-        method: this.method,
-        body: new FormData(this)
-    })
-    .then(response => {
-        if (response.ok) {
-        showSuccessMessage();
-        resetForm();
-        } else {
-        throw new Error('Try again later. ');
+
+    var inputs = document.getElementById("contactForm").querySelectorAll("input[type=text], input[type=email], textarea");
+    var allFieldsFilled = true;
+    inputs.forEach(function(input) {
+        if (!input.value.trim()) {
+            allFieldsFilled = false;
         }
-    })
-    .catch(error => console.error('Error:', error));
+    });
+    if (allFieldsFilled) {
+        fetch(this.action, {
+            method: this.method,
+            body: new FormData(this)
+        })
+        .then(response => {
+            if (response.ok) {
+                showSuccessMessage();
+                resetForm();
+            } else {
+                throw new Error('Try again later!');
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    } else {
+        alert("Please fill in all fields before submitting the form!");
+    }
 });
